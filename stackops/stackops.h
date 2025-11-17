0f5892e1b64801508e96d2c403303fbbae817191
ninetendo59 <hershel_tan@outlook.com>
2025-11-17 21:41:30 +0800

    fix norminette except header on stackops.h

--- stackops/stackops.h
+++ stackops/stackops.h
@@ -1,21 +1,19 @@
-#ifndef STACK_OPS_H
-#define STACK_OPS_H
+#ifndef STACKOPS_H
+# define STACKOPS_H
 
-#include "../libft/libft.h"
-#include <stdio.h>
+# include "../libft/libft.h"
+# include <stdio.h>
 
-extern int op_count;  // Declare the operation counter
-
-void swap_a(int *a, int len);
-void swap_b(int *b, int len);
-void swap_multi_stack(int **a, int **b, int len_a, int len_b);
-void push_a(int **a, int **b, int *len_a, int *len_b);
-void push_b(int **a, int **b, int *len_a, int *len_b);
-void rotate_a(int *a, int len);
-void rotate_b(int *b, int len);
-void rotate_multi_stack(int **a, int **b, int *len_a, int *len_b);
-void rotate_reverse_a(int *a, int len);
-void rotate_reverse_b(int *b, int len);
-void rotate_reverse_multi_stack(int **a, int **b, int *len_a, int *len_b);
+void	swap_a(int *a, int len);
+void	swap_b(int *b, int len);
+void	swap_multi_stack(int **a, int **b, int len_a, int len_b);
+void	push_a(int **a, int **b, int *len_a, int *len_b);
+void	push_b(int **a, int **b, int *len_a, int *len_b);
+void	rotate_a(int *a, int len);
+void	rotate_b(int *b, int len);
+void	rotate_multi_stack(int **a, int **b, int *len_a, int *len_b);
+void	rotate_reverse_a(int *a, int len);
+void	rotate_reverse_b(int *b, int len);
+void	rotate_reverse_multi_stack(int **a, int **b, int *len_a, int *len_b);
 
 #endif
--- stackops/stackops_swap.c
+++ stackops/stackops_swap.c
@@ -10,42 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "stackops.h"
 
-
-void swap_a(int *a, int len) 
+void	swap_a(int *a, int len)
 {
+	int	temp;
+
 	printf("sa\n");
 	if (len < 2)
-		return;
-	int temp = a[0];
+		return ;
+	temp = a[0];
 	a[0] = a[1];
 	a[1] = temp;
 }
 
-void swap_b(int *b, int len)
+void	swap_b(int *b, int len)
 {
+	int	temp;
+
 	printf("sb\n");
 	if (len < 2)
-		return;
-	int temp = b[0];
+		return ;
+	temp = b[0];
 	b[0] = b[1];
 	b[1] = temp;
 }
 
-void swap_multi_stack(int **a, int **b, int len_a, int len_b)
+void	swap_multi_stack(int **a, int **b, int len_a, int len_b)
 {
+	int	temp_a;
+	int	temp_b;
+
 	printf("ss\n");
 	if (len_a < 2 || len_b < 2)
-		return;
-
-	int temp_a = (*a)[0];
+		return ;
+	temp_a = (*a)[0];
 	(*a)[0] = (*a)[1];
 	(*a)[1] = temp_a;
-
-	int temp_b = (*b)[0];
+	temp_b = (*b)[0];
 	(*b)[0] = (*b)[1];
 	(*b)[1] = temp_b;
 }
-
